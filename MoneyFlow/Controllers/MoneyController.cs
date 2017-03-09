d1c4a5c4c0cb08f5e0b32c5e4f606ecc5b819c8a
Vic Kuo <forsphone@gmail.com>
2017-03-09 22:12:24 +0800

    Add Accounting Book function.

---
+++ MoneyFlow/Controllers/MoneyController.cs
@@ -0,0 +1,42 @@
+﻿using MoneyFlow.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoneyFlow.Controllers
+{
+    public class MoneyController : Controller
+    {
+        // GET: Money
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AccountingBook()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult MoneyBasicInputForm()
+        {
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public ActionResult MoneyList()
+        {
+            var money = new List<MoneyModel>()
+            {
+                new MoneyModel() { Type = "支出", Date = new DateTime(2016, 01, 01), Money = 300 },
+                new MoneyModel() { Type = "支出", Date = new DateTime(2016, 01, 02), Money = 1600 },
+                new MoneyModel() { Type = "支出", Date = new DateTime(2016, 01, 03), Money = 300 }
+            };
+            return PartialView(money);
+        }
+
+    }
+}
---
+++ MoneyFlow/Models/MoneyModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoneyFlow.Models
+{
+    public class MoneyModel
+    {
+        public string Type { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Money { get; set; }
+    }
+}
